5f45f4ac3d67b306355431de284fafe30f868642
ichensky <ichensky@live.com>
2014-11-06 03:37:01 +0200

    add project

---
+++ FeedService/FeedService/FeedService.svc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web;
+
+namespace FeedService
+{
+    public class FeedService : IFeedService
+    {
+        public string GetData(string q, string num, string t)
+        {
+            var s = "";
+
+            var str = new StringBuilder("{\"responseData\": {\"xmlString\":\"");
+            str.Append(s);
+            str.Append("\"}, \"responseDetails\": null, \"responseStatus\": 200}");
+
+            if (WebOperationContext.Current != null)
+                WebOperationContext.Current.OutgoingResponse.ContentType = "text/javascript; charset=utf-8";
+
+            return str.ToString();
+        }
+    }
+}
---
+++ FeedService/FeedService/IFeedService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace FeedService
+{
+    [ServiceContract]
+    public interface IFeedService
+    {
+
+        /// <summary>
+        /// Gets the data.
+        /// </summary>
+        /// <param name="q">The q - the query consists of a URL.</param>
+        /// <param name="num">The number - Applicable to load feed only. This optional argument supplies the number of entries to load from the feed specified by q. A value of -1 indicates the maximum number of entries supported, currently 100. By default, load feed returns four results.</param>
+        /// <param name="t">The t.</param>
+        /// <returns></returns>
+        [OperationContract]
+        [WebInvoke(Method = "GET", BodyStyle  = WebMessageBodyStyle.Bare, ResponseFormat = WebMessageFormat.Json, UriTemplate = "load?q={q}&num={num}&t={t}")]
+        string GetData(string q, string num, string t);
+    }
+}
